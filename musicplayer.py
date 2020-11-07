b8baa4c5eae88ef480c84176152d60eecd54c736
ganesh kavhar <kavharg@gmail.com>
2020-11-07 10:13:04 +0530

    Update musicplayer.py

--- musicplayer.py
+++ musicplayer.py
@@ -23,7 +23,7 @@ def resumesong():
     mixer.music.unpause()    
 
 root=Tk()
-root.title('ProjectGurukul Music player project')
+root.title('Super Music player')
 
 mixer.init()
 songstatus=StringVar()
